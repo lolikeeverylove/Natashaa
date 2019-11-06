13119dd7401a40da3536748efac02a9c3cda3560
lolike <girev.alex@yandex.ru>
2019-11-06 22:24:00 +0300

    darov second

---
+++ app/src/main/java/com/example/myapplication/splashActivity.java
@@ -0,0 +1,44 @@
+package com.example.myapplication;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class splashActivity extends Activity {
+
+    private final int SPLASH_DISPLAY_LENGHT = 5000;
+
+
+
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main3);
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+
+            public void run() {
+
+                Intent mainIntent = new Intent(splashActivity.this, MainActivity.class);
+
+                splashActivity.this.startActivity(mainIntent);
+
+                splashActivity.this.finish();
+
+            }
+
+        }, SPLASH_DISPLAY_LENGHT);
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+    }
+}
